8883e5b227623475d5c79c573f30719e27297cb2
Munsterberg <jetrussell93@gmail.com>
2016-02-20 21:32:07 -0500

    Creating Comment model and migrating

--- movies/models.py
+++ movies/models.py
@@ -5,6 +5,17 @@ class Movie(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     title = models.CharField(max_length=150)
     description = models.TextField()
+    image = models.CharField(max_length=255)
 
     def __str__(self):
         return self.title
+
+
+class Comment(models.Model):
+    author = models.CharField(max_length=255)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    movie = models.ForeignKey(Movie)
+
+    def __str__(self):
+        return self.author
