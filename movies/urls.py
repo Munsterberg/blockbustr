dbca47bdfa5d4c931bb92bec376ca89cd80c3076
Munsterberg <jetrussell93@gmail.com>
2016-02-20 22:20:37 -0500

    Adding links to show route

--- movies/urls.py
+++ movies/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.movie_list),
-    url(r'(?P<pk>\d+)/$', views.show)
+    url(r'^$', views.movie_list, name='list'),
+    url(r'(?P<pk>\d+)/$', views.show, name='show'),
 ]
