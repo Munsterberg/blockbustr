08246e3db210657198361dccabbb538f948c80c3
Munsterberg <jetrussell93@gmail.com>
2016-02-20 20:15:40 -0500

    Adding urls and creating movies_list view

--- movies/views.py
+++ movies/views.py
@@ -1,3 +1,10 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Movie
+
 
 # Create your views here.
+def movie_list(request):
+    movies = Movie.objects.all()
+    data = ', '.join([str(movie) for movie in movies])
+    return HttpResponse(data)
