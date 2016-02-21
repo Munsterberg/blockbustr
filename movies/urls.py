3114dff57834cc14e3db240f2e1a3aa0fe205e79
Munsterberg <jetrussell93@gmail.com>
2016-02-20 21:48:50 -0500

    Creating show controller and view

--- movies/urls.py
+++ movies/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.movie_list)
+    url(r'^$', views.movie_list),
+    url(r'(?P<pk>\d+)/$', views.show)
 ]
--- movies/views.py
+++ movies/views.py
@@ -7,3 +7,7 @@ from .models import Movie
 def movie_list(request):
     movies = Movie.objects.all()
     return render(request, 'movies/index.html', { 'movies': movies })
+
+def show(request, pk):
+    movie = Movie.objects.get(pk=pk)
+    return render(request, 'movies/show.html', { 'movie': movie })
